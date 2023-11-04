e17b80e12025171b4ae7ec2a9f9310b37abc4783
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-11-04 17:35:38 -0500

    Sick gradient, fixed the green color scheme and changed some other UI things

--- finteach/lib/Presentation/lesson_page.dart
+++ finteach/lib/Presentation/lesson_page.dart
@@ -8,62 +8,72 @@ class LessonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 133, 231, 136), Colors.lightGreen],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: Column(
+        children: [
+          // Top bar with white background
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildCloseButton(context),
+                Expanded(
+                  child: _buildProgressBar(),
+                ),
+                _buildHeartCount(context, heartCount: 5),
+              ],
+            ),
           ),
-        ),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              // Top row with back button, progress bar, and hearts
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _buildCloseButton(context),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.green, // Lighter green gradient
+                    Color(0xFF11825C)
+                  ], // Darker green gradient
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: SafeArea(
+                top: false, // SafeArea is now only needed for bottom part
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    SizedBox(height: 24),
+                    _buildCard(context, 'Question 1 of 5'),
+                    _buildCard(context,
+                        'This is an example question, what is the answer for it?'),
+                    SizedBox(height: 24),
                     Expanded(
-                      child: _buildProgressBar(),
+                      child: ListView(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        children: [
+                          _buildOption(context, 'Option 1'),
+                          _buildOption(context, 'Option 2'),
+                          _buildOption(context, 'Option 3'),
+                          _buildOption(context, 'Option 4'),
+                        ],
+                      ),
                     ),
-                    _buildHeartCount(context, heartCount: 5),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0),
+                      child: ElevatedButton(
+                        child: Text('Next Question'),
+                        onPressed: () {
+                          // Handle next question action
+                        },
+                      ),
+                    ),
                   ],
                 ),
               ),
-              SizedBox(height: 24),
-              _buildCard(context, 'Question 1 of 5'),
-              _buildCard(context,
-                  'This is an example question, what is the answer for it?'),
-              SizedBox(height: 24),
-              Expanded(
-                child: ListView(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  children: [
-                    _buildOption(context, 'Option 1'),
-                    _buildOption(context, 'Option 2'),
-                    _buildOption(context, 'Option 3'),
-                    _buildOption(context, 'Option 4'),
-                  ],
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  child: Text('Next Question'),
-                  onPressed: () {
-                    // Handle next question action
-                  },
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -90,7 +100,7 @@ class LessonPage extends StatelessWidget {
       children: [
         Icon(Icons.favorite, color: Colors.red),
         SizedBox(width: 8),
-        Text('$heartCount', style: TextStyle(color: Colors.green)),
+        Text('$heartCount', style: TextStyle(color: Colors.black)),
       ],
     );
   }
