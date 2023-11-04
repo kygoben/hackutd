c7f4a9935ec52519baad51a1b80e76b773574b34
Andrew Ritchie <ritchiea@iastate.edu>
2023-11-04 16:59:47 -0500

    Random Icon assignments for list views

--- finteach/lib/Application/constants.dart
+++ finteach/lib/Application/constants.dart
@@ -1,4 +1,6 @@
 import 'package:finteach/Domain/chapter.dart';
+import 'package:flutter/material.dart';
+
 
 
 
@@ -9,5 +11,26 @@ List <Chapters> chaptersList = [
   Chapters(number: '4', name: 'State Management'),
   Chapters(number: '5', name: 'Navigation and Routing'),
 
+];
+
+const List<IconData> sampleFinanceIcons = [
+  Icons.account_balance,
+  Icons.account_balance_wallet,
+  Icons.monetization_on,
+  Icons.attach_money,
+  Icons.trending_up,
+  Icons.bar_chart,
+  Icons.pie_chart,
+  Icons.savings,
+  Icons.credit_score,
+  Icons.currency_exchange,
+  Icons.business_center,
+  Icons.store,
+  Icons.shopping_cart,
+  Icons.paid,
+  Icons.receipt,
+  Icons.analytics,
+  Icons.percent,
+  Icons.insights,
 ];
 
--- finteach/lib/Presentation/app.dart
+++ finteach/lib/Presentation/app.dart
@@ -1,42 +1,39 @@
+import 'dart:math';
+import 'package:finteach/Presentation/lesson_page.dart';
 import 'package:finteach/Presentation/practice_page.dart';
 import 'package:flutter/material.dart';
 import 'package:finteach/Application/constants.dart';
-import 'package:finteach/Presentation/lesson_page.dart';
 
 class ModuleList extends StatelessWidget {
+  final Random random = Random();
+
+  ModuleList({Key? key}) : super(key: key);
+    final List<IconData> financeIcons= sampleFinanceIcons; // List of icons
+
   @override
   Widget build(BuildContext context) {
-    // Initialize an empty list of widgets.
-    List<Widget> moduleTilesWithDividers = [];
-
-    // Add ModuleTile and Divider to the list for each chapter.
-    chaptersList.forEach((chapter) {
-      // Add ModuleTile.
-      moduleTilesWithDividers.add(
-        ModuleTile(
-          title: 'Module ${chapter.number}',
-          subtitle: chapter.name,
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LessonPage(moduleNumber: chapter.number,)), // Replace with actual lesson page
-            );
-          },
-        ),
+    List<Widget> moduleTiles = chaptersList.map((chapter) {
+      // Get a random icon from the list
+      IconData icon = financeIcons[random.nextInt(financeIcons.length)];
+      return ModuleTile(
+        icon: icon,
+        title: 'Module ${chapter.number}',
+        subtitle: chapter.name,
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => LessonPage(moduleNumber: chapter.name)), // Replace with actual lesson page
+          );
+        },
       );
-
-      // Add a Divider for each ModuleTile except for the last one.
-      if (chapter != chaptersList.last) {
-        moduleTilesWithDividers.add(Divider());
-      }
-    });
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Modules'),
       ),
       body: ListView(
-        children: moduleTilesWithDividers,
+        children: moduleTiles,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -53,14 +50,16 @@ class ModuleList extends StatelessWidget {
 }
 
 class ModuleTile extends StatelessWidget {
+  final IconData icon;
   final String title;
   final String subtitle;
   final VoidCallback onTap;
 
   const ModuleTile({
-    Key? key, 
-    required this.title, 
-    required this.subtitle, 
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
     required this.onTap,
   }) : super(key: key);
 
@@ -69,10 +68,11 @@ class ModuleTile extends StatelessWidget {
     return Card(
       margin: const EdgeInsets.all(8.0),
       child: ListTile(
+        leading: Icon(icon), // Insert icon here
         title: Text(title),
         subtitle: Text(subtitle),
-        trailing: Icon(Icons.arrow_forward_ios), // Add an arrow icon here
-        onTap: onTap, // Use the onTap callback
+        trailing: Icon(Icons.arrow_forward_ios),
+        onTap: onTap,
       ),
     );
   }
