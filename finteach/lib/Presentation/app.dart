88858ac188f587f3bafd9b975afc9ad8fbb2ca0b
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-11-05 01:35:27 -0600

    Modified bottom right button to use person / profile icon instead of edit icon

--- finteach/lib/Presentation/app.dart
+++ finteach/lib/Presentation/app.dart
@@ -21,7 +21,17 @@ class ModuleList extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Modules', style: TextStyle(color: Colors.black)),
+        title: Text('FinTeach',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 24.0,
+              letterSpacing:
+                  1.2, // Adjust the letter spacing for a logo-like effect
+              fontFamily:
+                  'YourCustomFont', // Replace with your custom font name
+            )),
+        centerTitle: true, // This centers the title in the AppBar
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
       ),
@@ -49,7 +59,7 @@ class ModuleList extends StatelessWidget {
             MaterialPageRoute(builder: (context) => PracticePage()),
           );
         },
-        child: Icon(Icons.edit, color: Colors.black),
+        child: Icon(Icons.person, color: Colors.black),
         backgroundColor: Colors.white,
         tooltip: 'Practice',
       ),
@@ -73,7 +83,7 @@ class ModuleTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        // Route to the LessonPage :)
+        // Route to the LessonPage
         Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) => LessonPage(title: title, chapter: chapter),
@@ -84,8 +94,7 @@ class ModuleTile extends StatelessWidget {
         margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius:
-              BorderRadius.circular(16), // Rounded corners for the box, pog
+          borderRadius: BorderRadius.circular(16),
           boxShadow: [
             BoxShadow(
               color: Colors.black26,
@@ -95,8 +104,7 @@ class ModuleTile extends StatelessWidget {
           ],
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment
-              .start, // Items get aligned to the start of the row with this bad boy
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -106,7 +114,36 @@ class ModuleTile extends StatelessWidget {
               child: Text(subtitle,
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             ),
-            const SizedBox(width: 16), // For extra some spacing at the end :)
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.green, // Green circle
+                shape: BoxShape.circle, // Circular shape
+              ),
+              margin: const EdgeInsets.only(right: 16.0), // Right margin
+              padding: const EdgeInsets.all(
+                  8.0), // Add padding to make the circle larger
+              child: IconButton(
+                icon: Icon(Icons.arrow_forward_ios,
+                    color: Colors.white,
+                    size: 24.0), // White arrow icon with size specified
+                onPressed: () {
+                  // Route to the LessonPage
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          LessonPage(title: title, chapter: chapter),
+                    ),
+                  );
+                },
+                tooltip: 'Go to module', // Tooltip text
+                padding: EdgeInsets.zero, // Reduces the default padding
+                constraints: BoxConstraints(
+                  minWidth: 48, // Minimum width for the IconButton
+                  minHeight: 48, // Minimum height for the IconButton
+                ), // Use constraints to set the size
+              ),
+            ),
+            const SizedBox(width: 16), // For some spacing at the end
           ],
         ),
       ),
