cff47c6a3f29599a72f02630b215265232c91ccc
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-11-04 23:17:00 -0500

    Finally fixed weird scrolling bug holy moly

--- finteach/lib/Presentation/app.dart
+++ finteach/lib/Presentation/app.dart
@@ -102,79 +102,87 @@ class _ModuleTileState extends State<ModuleTile> {
     var offset = renderBox.localToGlobal(Offset.zero);
 
     return OverlayEntry(
-      builder: (context) => Stack(
-        children: [
-          Positioned(
-            left: offset.dx,
-            top: offset.dy +
-                size.height +
-                20.0, // Space for the speech bubble tail
-            width: size.width,
-            child: Material(
-              elevation: 4.0,
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(
-                      8), // Rounded corners for the pop-up box
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(widget.title,
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    SizedBox(height: 8),
-                    Text(widget.subtitle),
-                    SizedBox(height: 16), // More space before the button
-                    ElevatedButton(
-                      onPressed: () {
-                        _overlayEntry?.remove();
-                        _overlayEntry = null;
+      builder: (context) => GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          _overlayEntry?.remove();
+          _overlayEntry = null;
+        },
+        child: Stack(
+          children: [
+            Positioned(
+              left: offset.dx,
+              top: offset.dy +
+                  size.height +
+                  20.0, // Space for the speech bubble tail
+              width: size.width,
+              child: Material(
+                elevation: 4.0,
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                        8), // Rounded corners for the pop-up box, jank fix
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(widget.title,
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      SizedBox(height: 8),
+                      Text(widget.subtitle),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          _overlayEntry?.remove();
+                          _overlayEntry = null;
 
-                        // Here we push the LessonPage onto the navigation stack
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                LessonPage(moduleNumber: widget.title),
-                          ),
-                        );
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 24.0, vertical: 12.0), // Bigger button
-                        child: Text('START',
-                            style: TextStyle(fontSize: 20)), // Larger text
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green, // Button background color
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
+                          // Here we push the LessonPage onto the navigation stack
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  LessonPage(moduleNumber: widget.title),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 24.0,
+                              vertical: 12.0), // Bigger button
+                          child: Text('START',
+                              style: TextStyle(fontSize: 20)), // Larger text
                         ),
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 32, vertical: 16), // Larger padding
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green, // Button background color
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 32, vertical: 16), // Larger padding
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            left: offset.dx +
-                (size.width / 2) -
-                10, // Adjust this to center the tail
-            top: offset.dy + size.height,
-            child: Container(
-              width: 30, // Width of the tail
-              height: 20, // Height of the tail
-              child: CustomPaint(
-                painter: _TrianglePainter(),
+            Positioned(
+              left: offset.dx +
+                  (size.width / 2) -
+                  10, // Adjust this to center the tail
+              top: offset.dy + size.height,
+              child: Container(
+                width: 30, // Width of the tail
+                height: 20, // Height of the tail
+                child: CustomPaint(
+                  painter: _TrianglePainter(),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
